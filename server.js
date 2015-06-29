f2eafcf1948dba259e912d71f00b6a2459737fbb
kafoso <soefritz@gmail.com>
2015-06-29 14:28:20 +0200

    Moved some logic to bootstrap

--- server.js
+++ server.js
@@ -3,34 +3,17 @@ var express = require('express');
 var passport = require('passport');
 var bootstrap = require(__dirname + '/server/bootstrap.js');
 
+var OAuthAccessToken; // TODO: Move to db
+
 var app = express();
-var PORT = process.env.PORT || 443;
-
-var OAuthAccessToken;
-
-// Bootstrap models
-fs.readdirSync(__dirname + '/server/mvc/models').forEach(function (file) {
-  if (~file.indexOf('.js')) require(__dirname + '/server/mvc/models/' + file);
-});
-
-// Bootstrap passport config
-console.log("Configuring passport");
-require(__dirname + '/server/bootstrap/passport.js')(passport);
-
-// Bootstrap application settings
-console.log("Configuring express");
-require(__dirname + '/server/bootstrap/express.js')(app, passport);
-
-// Bootstrap routes
-console.log("Configuring routes");
-require(__dirname + '/server/bootstrap/routes.js')(app, passport);
+bootstrap.startup(app, passport);
 
 // Create an HTTPS service
 var https = require('https');
 var server = https.createServer({
   key: fs.readFileSync(__dirname + process.env.SSL_KEY),
   cert: fs.readFileSync(__dirname + process.env.SSL_CERTIFICATE)
-}, app).listen(PORT, process.env.HOST);
+}, app).listen(process.env.PORT, process.env.HOST);
 
 // Fallback: HTTP service which redirects to HTTPS
 var http = require('http');
@@ -39,4 +22,4 @@ http.createServer(function(req, res){
   res.end();
 }).listen(80, process.env.HOST);
 
-console.log('Secure server running at https://'+process.env.HOST+'/ (port: '+PORT+')');
+console.log('Secure server running at https://'+process.env.HOST+'/ (port: '+process.env.PORT+')');
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -10,3 +10,19 @@ Object.keys(env).forEach(function(key){
   process.env[key] = env[key];
 });
 process.env.ROOT = path.normalize(__dirname + '/..');
+
+exports.startup = function(app, passport){
+  console.log("Boostrap: Setting up autoload models");
+  fs.readdirSync(process.env.ROOT + '/server/mvc/models').forEach(function (file) {
+    if (~file.indexOf('.js')) require(process.env.ROOT + '/server/mvc/models/' + file);
+  });
+
+  console.log("Boostrap: Configuring passport");
+  require(process.env.ROOT + '/server/bootstrap/passport.js')(passport);
+
+  console.log("Boostrap: Configuring express");
+  require(process.env.ROOT + '/server/bootstrap/express.js')(app, passport);
+
+  console.log("Boostrap: Configuring routes");
+  require(process.env.ROOT + '/server/bootstrap/routes.js')(app, passport);
+};
