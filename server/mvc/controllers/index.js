e5962977434402e3ba4c7d4f246203364068f5ec
kafoso <soefritz@gmail.com>
2015-06-29 17:03:49 +0200

    Title and heading is now just 'Starbow'

--- server/mvc/controllers/index.js
+++ server/mvc/controllers/index.js
@@ -1,13 +1,13 @@
 exports.index = function(req, res){
   if(req.isAuthenticated()) {
     res.render('indexAuthenticated', {
-      title: 'Starbow Bnet',
+      title: 'Starbow',
       userId: req.user.id,
       battletag: req.user.battletag
     });
   } else {
     res.render('index', {
-      title: 'Starbow Bnet'
+      title: 'Starbow'
     });
   }
 };
