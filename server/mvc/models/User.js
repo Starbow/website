5e3a2a7022c116f969d127b451e39df14448ffe3
kafoso <soefritz@gmail.com>
2015-06-30 13:49:53 +0200

    Completely refactored the 'User' model
    It now uses 'thinky' and 'rethinkdbdash'.

--- server/mvc/models/User.js
+++ server/mvc/models/User.js
@@ -1,41 +1,118 @@
-var _rethinkdb,
-    _name = module.filename.slice(module.filename.lastIndexOf('/')+1, module.filename.length).replace(/\.(.*?)$/, '');
+var Promise = require("bluebird");
+var Cryptr = require("cryptr");
+
+var thinky, r;
+var ThinkyUserModel;
 
 Models.User = function(){
-  var rethinkdb = _rethinkdb;
-  var name = _name;
-  var dbContents = null;
+  var self = this,
+    document = new ThinkyUserModel({}),
+    cryptr = null,
+    exists = false;
 
-  this.findByUserId = function(userId, callback){
-    dbContents = null;
-    console.log("User.findByUserId("+userId+")");
-    onConnect(function(err, connection){
-      rethinkdb
-        .db(process.env.DB_NAME)
-        .table('users')
-        .filter({"userId":userId})
+  this.findByUserId = function(userId){
+    exists = false;
+    return new Promise(function(resolve, reject){
+      ThinkyUserModel
+        .filter({"userId": userId})
+        .orderBy(r.desc("timeCreated"))
         .limit(1)
-        .run(connection)
-        .then(function(result){
-          dbContents = result;
+        .run()
+        .then(function(docs){
+          if (Array.isArray(docs) && docs.length) {
+            exists = true;
+            document = docs[0];
+          }
+          return resolve();
         })
-        .error(function(err){
-          logerror("[ERROR][%s][findUserById] %s:%s\n%s", connection['_id'], err.name, err.msg, err.message);
-        })
-        .finally(function(){
-          connection.close();
-          callback(null, dbContents);
-        });
+        .error(reject);
     });
   };
-  this.getObjectName = function(){
-    return name;
+  this.save = function(){
+    exists = false;
+    return new Promise(function(resolve, reject){
+      document.merge({"timeModified": r.now()});
+      try {
+        document.validate();
+      } catch (e) {
+        return reject(e);
+      }
+      document
+        .save()
+        .then(function(){
+          exists = true;
+          return resolve();
+        })
+        .error(reject);
+    });
+  };
+  this.setValue = function(key, value){
+    var obj = {};
+    obj[key] = value;
+    document.merge(obj);
+    document.validate();
+    return this;
+  };
+  this.setValues = function(obj){
+    document.merge(obj);
+    document.validate();
+    return this;
+  };
+  this.getValue = function(key){
+    var values = self.getValues();
+    if (values.hasOwnProperty(key)) {
+      return values[key];
+    }
+    return null;
+  };
+  this.getValues = function(){
+    var clone = {};
+    for (var i in document) {
+      if (document.hasOwnProperty(i))
+        clone[i] = document[i];
+    }
+    return clone;
   };
   this.isValid = function(){
-    return (typeof(dbContents) == 'object' && dbContents !== null);
+    try {
+      document.validate();
+    } catch (e) {
+      return false;
+    }
+    return true;
+  };
+  this.exists = function(){
+    return exists;
+  };
+  this.updateTimeLatestLogin = function(){
+    document.timeLatestLogin = r.now();
+    return this;
+  };
+  this.encryptOauthToken = function(token){
+    return self.getCryptr().encrypt(token);
+  };
+  this.decryptOauthToken = function(encryptedToken){
+    return self.getCryptr().decrypt(encryptedToken);
+  };
+  this.getCryptr = function(){
+    if (cryptr === null) {
+      cryptr = new Cryptr(process.env.BNET_OAUTH_TOKEN_ENCRYPTION_SALT);
+    }
+    return cryptr;
   };
 };
 
-module.exports = function(rethinkdb) {
-  _rethinkdb = rethinkdb;
+module.exports = function(dbConfig) {
+  thinky = require("thinky")(dbConfig);
+  r = thinky.r;
+  var type = thinky.type;
+  ThinkyUserModel = thinky.createModel("users", {
+    userId: type.number().integer().default(null).min(1).required().allowNull(false),
+    oauthType: type.string().default(null).min(1).required().allowNull(false),
+    oauthTokenEncrypted: type.string().default(null).min(1).required().allowNull(false),
+    timeCreated: type.date().default(r.now()).required().allowNull(false),
+    timeModified: type.date().default(r.now()).required().allowNull(false),
+    timeLatestLogin: type.date().default(r.now()).required().allowNull(false),
+  });
+  ThinkyUserModel.ensureIndex("timeCreated");
 };
