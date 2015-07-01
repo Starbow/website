92db1a99ff9dee6170a3524b357c052fdebf8a89
kafoso <soefritz@gmail.com>
2015-07-01 13:03:44 +0200

    Simplified paths

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -14,7 +14,6 @@ Object.keys(env).forEach(function(key){
 process.env.ROOT = path.normalize(__dirname + '/..');
 
 exports.startup = function(app, passport){
-  var dbConfig = require(process.env.ROOT + '/server/bootstrap/config/db/rethinkdb.js');
 
   console.log("Bootstrap: Setting up autoload models");
   var modelsPath = '/server/mvc/models';
@@ -24,13 +23,14 @@ exports.startup = function(app, passport){
       require(process.env.ROOT + modelsPath + '/' + file)(dbConfig.config());
     }
   });
+  var dbConfig = require('./bootstrap/config/db/rethinkdb.js');
 
   console.log("Bootstrap: Configuring passport");
-  require(process.env.ROOT + '/server/bootstrap/passport.js')(passport);
+  require('./bootstrap/passport.js')(passport);
 
   console.log("Bootstrap: Configuring express");
-  require(process.env.ROOT + '/server/bootstrap/express.js')(app, passport);
+  require('./bootstrap/express.js')(app, passport);
 
   console.log("Bootstrap: Configuring routes");
-  require(process.env.ROOT + '/server/bootstrap/routes.js')(app, passport);
+  require('./bootstrap/routes.js')(app, passport);
 };
