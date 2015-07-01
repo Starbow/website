f07885db06f4b5640ef214ccdf5b27f813a2dce3
kafoso <soefritz@gmail.com>
2015-07-01 13:01:27 +0200

    Moved some auth logic around

--- server/mvc/controllers/index.js
+++ server/mvc/controllers/index.js
@@ -1,19 +1,21 @@
 exports.index = function(req, res){
-  if(req.isAuthenticated()) {
-    res.render('indexAuthenticated', {
-      title: 'Starbow',
-      userId: req.user.id,
-      battletag: req.user.battletag
-    });
-  } else {
-    res.render('index', {
+  if(!req.isAuthenticated()) {
+    return res.render('index', {
       title: 'Starbow'
     });
   }
+  res.render('indexAuthenticated', {
+    title: 'Starbow',
+    userId: req.user.id,
+    battletag: req.user.battletag
+  });
 };
 
 exports.userstuff = function(req, res){ // TODO: Temporary function for hacking away at user stuff
   var user = new Models.User;
+  if (!req.isAuthenticated()) {
+    return res.send('Not authenticated');
+  }
   user.findByUserId(req.user.id)
     .then(function(){
       console.log("user.getValues()", user.getValues());
