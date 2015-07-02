f7d5fac1fce95b94c6efa22f4f18a809b2d69d88
kafoso <soefritz@gmail.com>
2015-07-02 10:21:58 +0200

    Added try-catch and more helpful errors

--- server/bootstrap/config/env.js
+++ server/bootstrap/config/env.js
@@ -1,14 +1,23 @@
 var fs = require("fs");
 var path = require("path");
+var sprintf = require("sprintf-js").sprintf;
 
 module.exports = function(rootDir){
   rootDir = path.normalize(rootDir);
 
   var env = {};
-  var envFile = rootDir + '/server/bootstrap/config/env/env.json';
+  var envFile = __dirname + '/env/env.json';
 
-  env = fs.readFileSync(envFile, 'utf-8');
-  env = JSON.parse(env);
+  try {
+    env = fs.readFileSync(envFile, 'utf-8');
+  } catch (e) {
+    throw new Error(sprintf("You don't have an 'env.json' file in '%s'", __dirname + "/env"));
+  }
+  try {
+    env = JSON.parse(env);
+  } catch (e) {
+    throw new Error("Your 'env.json' file contains invalid JSON");
+  }
   Object.keys(env).forEach(function(key){
     process.env[key] = env[key];
   });
