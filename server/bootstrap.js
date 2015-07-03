27a649bec28008b4e3d79c281eb777020d6751de
kafoso <soefritz@gmail.com>
2015-07-03 12:04:59 +0200

    Wrapped bootstrap entries in try-catch

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -16,16 +16,23 @@ exports.startup = function(app, passport){
   console.log("Bootstrap: Configuring logs");
   var logs = require('./bootstrap/logs.js')(config);
 
-  console.log("Bootstrap: Configuring passport");
-  require('./bootstrap/passport.js')(passport);
+  try {
+    console.log("Bootstrap: Configuring passport");
+    require('./bootstrap/passport.js')(passport);
 
-  console.log("Bootstrap: Configuring express");
-  require('./bootstrap/express.js')(app, logs, passport);
+    console.log("Bootstrap: Configuring express");
+    require('./bootstrap/express.js')(app, logs, passport);
 
-  console.log("Bootstrap: Configuring models");
-  var thinky = require("thinky")(config.db.thinky);
-  require('./mvc/models.js')(config, thinky);
+    console.log("Bootstrap: Configuring models");
+    var thinky = require("thinky")(config.db.thinky);
+    require('./mvc/models.js')(config, thinky);
 
-  console.log("Bootstrap: Configuring routes");
-  require('./bootstrap/routes.js')(app, logs, passport);
+    console.log("Bootstrap: Configuring routes");
+    require('./bootstrap/routes.js')(app, logs, passport);
+  } catch (e) {
+    var errorType = (e instanceof Error) ? "Error" : "Exception";
+    logs.error.error('Uncaught ' + errorType + ":", e);
+    process.exit(); // Crash and burn; learn2code
+    return;
+  }
 };
