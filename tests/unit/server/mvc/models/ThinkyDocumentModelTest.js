b77ae433dd49d78d356d1dbd548a8e27fc63bc0c
kafoso <soefritz@gmail.com>
2015-07-14 00:51:44 +0200

    Added a bunch more tests

--- tests/unit/server/mvc/models/ThinkyDocumentModelTest.js
+++ tests/unit/server/mvc/models/ThinkyDocumentModelTest.js
@@ -37,7 +37,7 @@ describe("ThinkyDocumentModel", function(){
   });
   describe("save()", function(){
     it("Should not save an invalid document", function(done){
-      var thinkyDocumentModel = new ThinkyDocumentModel;
+      var thinkyDocumentModel = new ThinkyDocumentModel(undefined);
       thinkyDocumentModel
         .save()
         .error(function(err){
@@ -45,6 +45,54 @@ describe("ThinkyDocumentModel", function(){
           assert.equal(err.toString(), "TypeError: Cannot read property 'validate' of undefined");
           done();
         });
+    });
+    it("Should be able to save an valid document", function(done){
+      var ThinkyModel = bogusThinky.createModel("ThinkyDocumentModel_807ade1f", {});
+      var thinkyDocumentModel = new ThinkyDocumentModel(new ThinkyModel({}));
+      thinkyDocumentModel
+        .save()
+        .then(done);
+    });
+  });
+  describe("Values", function(){
+    var ThinkyModel;
+    var getThinkyModel = function(){
+      if (ThinkyModel === undefined) {
+        ThinkyModel = bogusThinky.createModel("ThinkyDocumentModel_eacd6576", {
+          foo: bogusThinky.type.string().default(null).min(1).required().allowNull(false)
+        });
+      }
+      return ThinkyModel;
+    };
+    describe("setValue()", function(){
+      it("Cannot set value for invalid key", function(){
+        var ThinkyModel = getThinkyModel();
+        var thinkyDocumentModel = new ThinkyDocumentModel(new ThinkyModel({}));
+        assert.throws(function(){thinkyDocumentModel.setValue("bar", "abc");}, Error);
+      });
+      it("Can set value for valid key", function(){
+        var ThinkyModel = getThinkyModel();
+        var thinkyDocumentModel = new ThinkyDocumentModel(new ThinkyModel({}));
+        thinkyDocumentModel.setValue("foo", "abc");
+      });
+    });
+    describe("setValues()", function(){
+      it("Must receive a key-value paired Object", function(){
+        var ThinkyModel = getThinkyModel();
+        var thinkyDocumentModel = new ThinkyDocumentModel(new ThinkyModel({}));
+        var notAnObject = "foo";
+        assert.throws(function(){thinkyDocumentModel.setValues(notAnObject);}, Error);
+      });
+      it("Cannot set invalid value", function(){
+        var ThinkyModel = getThinkyModel();
+        var thinkyDocumentModel = new ThinkyDocumentModel(new ThinkyModel({}));
+        assert.throws(function(){thinkyDocumentModel.setValues({"foo": 123});}, Error);
+      });
+      it("Can set value for valid key", function(){
+        var ThinkyModel = getThinkyModel();
+        var thinkyDocumentModel = new ThinkyDocumentModel(new ThinkyModel({}));
+        thinkyDocumentModel.setValues({"foo": "abc"});
+      });
     });
   });
 });
