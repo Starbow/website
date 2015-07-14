22e1ca1f57bf6030722fdc752649bb785fba0db9
kafoso <soefritz@gmail.com>
2015-07-14 12:13:34 +0200

    Now inherits 'DefaultModel'

--- server/mvc/models/ConfigModel.js
+++ server/mvc/models/ConfigModel.js
@@ -4,10 +4,11 @@
  * A model from which the 'config' data for the application can be retrieved. Said data is injected.
  */
 var inherit = require("inherit");
+var DefaultModel = require("./DefaultModel");
 
 var config;
 
-module.exports = inherit({}, {
+module.exports = inherit(DefaultModel, {}, {
   getConfig: function(){
     return config;
   },
