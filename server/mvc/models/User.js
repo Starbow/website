394e8b3b5976853ddad456628fcaf9af134b55bd
kafoso <soefritz@gmail.com>
2015-07-15 17:42:33 +0200

    Added a lot of functionality to User model

--- server/mvc/models/User.js
+++ server/mvc/models/User.js
@@ -4,46 +4,53 @@ var inherit = require("inherit");
 var ThinkyDocumentModel = require("./ThinkyDocumentModel");
 var Promise = require("bluebird");
 var Cryptr = require("cryptr");
+var sprintf = require("sprintf-js").sprintf;
+var emailValidator = require("email-validator");
 
-var ThinkyModel;
-var getThinkyModel = function(thinky){
-  if (ThinkyModel === undefined) {
-    ThinkyModel = thinky.createModel("users", {
-      userId: thinky.type.number().integer().default(null).min(1).required().allowNull(false),
-      oauthType: thinky.type.string().default(null).min(1).required().allowNull(false),
-      oauthTokenEncrypted: thinky.type.string().default(null).min(1).required().allowNull(false),
-      timeCreated: thinky.type.date().default(thinky.r.now()).required().allowNull(false),
-      timeModified: thinky.type.date().default(thinky.r.now()).required().allowNull(false),
-      timeLatestLogin: thinky.type.date().default(thinky.r.now()).required().allowNull(false),
-    }, {
-      "pk": "userId"
-    });
-    ThinkyModel.ensureIndex("timeCreated");
-  }
-  return ThinkyModel;
-};
+var thinky = ThinkyDocumentModel.getThinky();
+var ThinkyModel = thinky.createModel("Users", {
+  userId: thinky.type.number().integer().default(null).min(1).required().allowNull(false),
+  email: thinky.type.string().default(null).min(5).required().allowNull(true).validator(function(_email){
+    return (
+      _email === null
+      || emailValidator.validate(_email)
+    );
+  }),
+  emailVerificationCode: thinky.type.string().default(null).min(1).required().allowNull(true),
+  emailVerificationTime: thinky.type.date().default(null).required().allowNull(true),
+  oauthType: thinky.type.string().default(null).min(1).required().allowNull(false),
+  oauthTokenEncrypted: thinky.type.string().default(null).min(1).required().allowNull(false),
+  timeCreated: thinky.type.date().default(thinky.r.now()).required().allowNull(false),
+  timeModified: thinky.type.date().default(thinky.r.now()).required().allowNull(false),
+  timeLatestLogin: thinky.type.date().default(thinky.r.now()).required().allowNull(false),
+  roles: thinky.type.array().default(function(){
+    return ["user"];
+  }).schema(thinky.type.string()).required().allowNull(false)
+}, {
+  "pk": "userId"
+});
+ThinkyModel.ensureIndex("timeCreated");
+ThinkyModel.ensureIndex("emailVerificationTime");
+ThinkyModel.pre("save", function(next){
+  this.timeModified = ThinkyDocumentModel.getThinky().r.now();
+  next();
+});
 
 module.exports = inherit(ThinkyDocumentModel, {
   __constructor: function(){
-    var ThinkyModel = getThinkyModel(ThinkyDocumentModel.getThinky());
     this.__base(new ThinkyModel({}));
   },
   findByUserId: function(userId){
-    var self = this;
-    return new Promise(function(resolve, reject){
-      ThinkyModel
-        .get(userId)
-        .run()
-        .then(function(doc){
-          self.document = doc;
-          return resolve();
-        })
-        .error(reject);
-    });
+    return this.findByFilter({userId: userId});
   },
-  save: function(){
-    this.document.merge({"timeModified": ThinkyDocumentModel.getThinky().r.now()});
-    return this.__base();
+  findByEmail: function(email){
+    return this.findByFilter({email: email});
+  },
+  findByEmailVerificationCode: function(emailVerificationCode){
+    return this.findByFilter(
+      {emailVerificationCode: emailVerificationCode},
+      {index: thinky.r.desc("emailVerificationTime")}
+    );
   },
   updateTimeLatestLogin: function(){
     this.document.timeLatestLogin = ThinkyDocumentModel.getThinky().r.now();
@@ -58,5 +65,43 @@ module.exports = inherit(ThinkyDocumentModel, {
     this.guardIsString(encryptedToken);
     var cryptr = new Cryptr(ThinkyDocumentModel.getConfig().auth.bnet.encryptionSalt);
     return cryptr.decrypt(encryptedToken);
-  }
+  },
+  generateEmailVerificationCode: function(){
+    this.guardExistsInDatabase();
+    var salt = ThinkyDocumentModel.getConfig().user.email.verificationCodeSalt;
+    var userIdZeroPadded = sprintf("%016f", this.document.userId);
+    var cryptr = new Cryptr(salt);
+    return cryptr.encrypt(userIdZeroPadded);
+  },
+  validateEmailVerificationCode: function(verificationCode){
+    return (verificationCode === this.generateEmailVerificationCode());
+  },
+  validateEmailAddress: function(email){
+    return emailValidator.validate(email);
+  },
+  isEmailValid: function(){
+    this.guardIsValid();
+    return (
+      this.validateIsString(this.document.email)
+      && this.validateEmailAddress(this.document.email)
+    )
+  },
+  isEmailVerified: function(){
+    this.guardExistsInDatabase();
+    return (
+      (this.document.emailVerificationTime instanceof Date)
+      && this.document.emailVerificationTime().getTime() > 0
+    );
+  },
+  isRegistrationComplete: function(){
+    this.guardExistsInDatabase();
+    return (
+      this.isEmailValid()
+      && this.isEmailVerified()
+    );
+  },
+  isAdmin: function(){
+    this.guardExistsInDatabase();
+    return (this.document.roles.indexOf("admin") > -1);
+  },
 });
--- tests/unit/server/mvc/models/UserTest.js
+++ tests/unit/server/mvc/models/UserTest.js
@@ -7,6 +7,11 @@ var modelsPath = __dirname + "/../../../../../server/mvc/models"
       bnet: {
         encryptionSalt: "encryption_salt_123"
       }
+    },
+    user: {
+      email: {
+        verificationCodeSalt: "7551d612f86269214fd94c498a47e4bf"
+      }
     }
   }
   , bogusThinky = require("thinky")({db: "bogus"})
@@ -22,8 +27,8 @@ describe("User", function(){
       done();
     };
     bogusThinky.r.tableList().run().then(function(tables){
-      if (tables.indexOf("users") <= -1) {
-        bogusThinky.r.tableCreate("users").run().then(function(){
+      if (tables.indexOf("Users") <= -1) {
+        bogusThinky.r.tableCreate("Users").run().then(function(){
           callback();
         });
       } else {
@@ -32,58 +37,107 @@ describe("User", function(){
     });
   });
   after(function(done){
-    bogusThinky.r.table("users").delete().run().then(function(){
+    bogusThinky.r.table("Users").delete().run().then(function(){
       done();
     });
   });
   beforeEach(function(done){
-    bogusThinky.r.table("users").delete().run().then(function(){
+    bogusThinky.r.table("Users").delete().run().then(function(){
       User = require(modelsPath + "/User");
       done();
     });
   });
 
-  describe("findByUserId()", function(){
-    it("Should not be able to find a non-existant user", function(done){
-      var user = new User();
-      user
-        .findByUserId(1)
-        .error(function(err){
-          assert.ok(!user.validateIsValid(), "Should not be valid");
-          assert.ok(!user.validateExistsInDatabase(), "Should not exist in database");
-          assert.typeOf(user.getValues(), "object", "Object should contain default values");
-          done();
-        });
+  describe("Saving", function(){
+    describe("save()", function(){
+      it("Should not be able to save invalid user", function(done){
+        var user = new User();
+        user
+          .save()
+          .error(function(err){
+            expect(err).to.be.an.instanceOf(Error);
+            expect(err.toString()).to.contain("Document failed validation");
+            assert.ok(!user.validateIsValid());
+            assert.ok(!user.validateExistsInDatabase());
+            done();
+          });
+      });
+      it("Should not be able to save when email is invalid", function(done){
+        var user = new User();
+        user
+          .setValues({
+            userId: 1,
+            oauthType: "bogus",
+            oauthTokenEncrypted: "123bogus"
+          });
+        user.document.email = "bogus"; // Hard-overriding the email value
+        user
+          .save()
+          .error(function(err){
+            expect(err).to.be.an.instanceOf(Error);
+            expect(err.toString()).to.contain("Document failed validation: Validator for the field [email] returned `false`.");
+            assert.ok(!user.validateIsValid());
+            assert.ok(!user.validateExistsInDatabase());
+            done();
+          });
+      });
+      it("Can save a valid user", function(done){
+        var user = new User();
+        user
+          .setValues({
+            userId: 1,
+            oauthType: "bogus",
+            oauthTokenEncrypted: "123bogus",
+          })
+          .save()
+          .then(function(){
+            assert.ok(user.validateIsValid());
+            assert.ok(user.validateExistsInDatabase());
+            assert.strictEqual(user.getValue('userId'), 1);
+            assert.strictEqual(user.getValue('oauthType'), "bogus");
+            assert.strictEqual(user.getValue('oauthTokenEncrypted'), "123bogus");
+            expect(user.getValue('timeModified')).to.be.an.instanceOf(Date);
+            done();
+          });
+      });
     });
   });
-  describe("save()", function(){
-    it("Should not be able to save invalid user", function(done){
-      var user = new User();
-      user
-        .save()
-        .error(function(err){
-          assert.ok(!user.validateIsValid(), "Should not be valid");
-          assert.ok(!user.validateExistsInDatabase(), "Should not exist");
-          done();
-        });
-    });
-    it("Can save a valid user", function(done){
-      var user = new User();
-      user
-        .setValues({
-          userId: 1,
-          oauthType: "bogus",
-          oauthTokenEncrypted: "123bogus",
-        })
-        .save()
-        .then(function(){
-          assert.ok(user.validateIsValid(), "Should be valid");
-          assert.ok(user.validateExistsInDatabase(), "Should exist");
-          assert.strictEqual(user.getValue('userId'), 1, "Value: userId");
-          assert.strictEqual(user.getValue('oauthType'), "bogus", "Value: oauthType");
-          assert.strictEqual(user.getValue('oauthTokenEncrypted'), "123bogus", "Value: oauthType");
-          done();
-        });
+  describe("Finding", function(){
+    describe("findByUserId()", function(){
+      it("Should not be able to find a non-existant user", function(done){
+        var user = new User();
+        user
+          .findByUserId(1)
+          .error(function(err){
+            expect(err).to.be.an.instanceOf(Error);
+            expect(err.toString()).to.contain("Document not found");
+            assert.ok(!user.validateIsValid());
+            assert.ok(!user.validateExistsInDatabase());
+            assert.typeOf(user.getValues(), "object", "Object should contain default values");
+            done();
+          });
+      });
+      it("Should be able to find an existing user", function(done){
+        var user = new User();
+        user
+          .setValues({
+            userId: 1,
+            oauthType: "bogus",
+            oauthTokenEncrypted: "123bogus",
+          })
+          .save()
+          .then(function(){
+            var subUser = new User();
+            subUser
+              .findByUserId(1)
+              .then(function(){
+                done();
+              })
+              .error(function(err){
+                console.log("err", err)
+              });
+          });
+      });
     });
   });
   describe("encryptOauthToken()", function(){
@@ -107,6 +161,75 @@ describe("User", function(){
       var encryptedToken = "41a65d5a9d2156f20daff6add137ea3465854d5c3c262ddb52ae64251d7a9d9e";
       var token = user.decryptOauthToken(encryptedToken);
       assert.strictEqual(token, "my_little_oauth_token");
+    });
+  });
+  describe("generateEmailVerificationCode()", function(){
+    it("Will fail if user does not exist in database", function(){
+      var user = new User();
+      assert.throw(function(){user.generateEmailVerificationCode()}, Error);
+    });
+    it("Will generate a unique verification code", function(done){
+      var user = new User();
+      user
+        .setValues({
+          userId: 1,
+          oauthType: "bogus",
+          oauthTokenEncrypted: "123bogus",
+        })
+        .save()
+        .then(function(){
+          var expected = "646df13627734b1f8a319a11e1c759be42a72cecd52ea57419cb22448db4ba61";
+          assert.strictEqual(user.generateEmailVerificationCode(), expected);
+          done();
+        });
+    });
+  });
+  describe("validateEmailVerificationCode()", function(){
+    it("Will fail if user does not exist in database", function(){
+      var user = new User();
+      assert.throw(function(){user.validateEmailVerificationCode("123")}, Error);
+    });
+    it("Will validate input code against a generated verification code", function(done){
+      var user = new User();
+      user
+        .setValues({
+          userId: 1,
+          oauthType: "bogus",
+          oauthTokenEncrypted: "123bogus",
+        })
+        .save()
+        .then(function(){
+          assert.ok(user.validateEmailVerificationCode("646df13627734b1f8a319a11e1c759be42a72cecd52ea57419cb22448db4ba61"));
+          assert.ok(!user.validateEmailVerificationCode("_646df13627734b1f8a319a11e1c759be42a72cecd52ea57419cb22448db4ba61"));
+          done();
+        });
+    });
+  });
+  describe("validateEmailAddress()", function(){
+    it("Will validate address patterns correctly", function(){
+      var user = new User();
+      assert.ok(user.validateEmailAddress("a@b.c"));
+      assert.ok(user.validateEmailAddress("uSeR@example.co.uk"));
+      assert.ok(!user.validateEmailAddress(null));
+      assert.ok(!user.validateEmailAddress("a @b.c"));
+    });
+  });
+  describe("isEmailValid()", function(){
+    it("Will fail if user is not valid", function(){
+      var user = new User();
+      assert.throw(function(){user.isEmailValid()}, Error);
+    });
+    it("Will correctly validate a valid email address", function(){
+      var user = new User();
+      user
+        .setValues({
+          userId: 1,
+          oauthType: "bogus",
+          oauthTokenEncrypted: "123bogus",
+        });
+      assert.ok(!user.isEmailValid());
+      user.setValue("email", "user@email.com");
+      assert.ok(user.isEmailValid());
     });
   });
 });
