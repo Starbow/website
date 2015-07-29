a05448bc50a15a1d95561675d99456e209dc771d
kafoso <soefritz@gmail.com>
2015-07-29 15:32:49 +0200

    Added nice Bootstrap theme and layout

--- server/mvc/views/layout/default.src.jsx
+++ server/mvc/views/layout/default.src.jsx
@@ -12,6 +12,8 @@ var DefaultLayout = React.createClass({
           <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
           <meta name="viewport" content="width=device-width,initial-scale=1"/>
           <link type="text/css" rel="stylesheet" href="/assets/cdn/bootstrap/3.3.1/css/bootstrap.min.css"/>
+          <link type="text/css" rel="stylesheet" href="/assets/framework/bootstrap/themes/starbow.css"/>
+          <link type="text/css" rel="stylesheet" href="/assets/layout/default.css"/>
         </head>
         <body>
           <page>
