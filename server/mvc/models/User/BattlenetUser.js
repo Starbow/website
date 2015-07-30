3ab3ba07c6999fc90a5fe56fdbde68ef1578b98c
kafoso <soefritz@gmail.com>
2015-07-30 14:12:29 +0200

    Corrected function parent

--- server/mvc/models/User/BattlenetUser.js
+++ server/mvc/models/User/BattlenetUser.js
@@ -8,7 +8,7 @@ var BattlenetUser = inherit(User, {
   fetchSC2Info: function(){
     var self = this;
     return new Promise(function(resolve, reject){
-      var oauthToken = self.decryptOauthToken(self.getValue('oauthTokenEncrypted'));
+      var oauthToken = User.decryptOauthToken(self.getValue('oauthTokenEncrypted'));
       var bnet = require('battlenet-api')(self.getConfig().auth.bnet.clientID);
       bnet.account.sc2({origin: 'eu', access_token: oauthToken}, function(err, response){
         if (err) {
