baa8afd15919bb183f9bf99b314d6a5ec9bd4e31
kafoso <soefritz@gmail.com>
2015-07-30 14:09:53 +0200

    Added 'battletag' to Users table

--- server/mvc/models/User/UserMapper.js
+++ server/mvc/models/User/UserMapper.js
@@ -7,6 +7,7 @@ var thinky = ThinkyDocumentModel.getThinky();
 
 var Model = thinky.createModel("Users", {
   userId: thinky.type.number().integer().default(null).min(1).required().allowNull(false),
+  battletag: thinky.type.string().default(null).min(1).required().allowNull(false),
   nickname: thinky.type.string().default(null).required().allowNull(true).validator(function(nickname){
     return (
       nickname === null
