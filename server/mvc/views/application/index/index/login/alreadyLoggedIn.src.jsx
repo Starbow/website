40ee72e9acffbed5fe3b29f57bcfc69146107b2a
kafoso <soefritz@gmail.com>
2015-07-30 18:32:58 +0200

    Added a nice message and a logout button

--- server/mvc/views/application/index/index/login/alreadyLoggedIn.src.jsx
+++ server/mvc/views/application/index/index/login/alreadyLoggedIn.src.jsx
@@ -8,7 +8,9 @@
       return (
         <DefaultLayout title={this.props.title} description={this.props.description}>
           <h1>{this.props.title}</h1>
-          <p>You are currently logged in as <a href={this.props.userProfileURL}>{this.props.userNickname}</a></p>
+          <p>You are currently logged in as <strong><a href={this.props.userProfileURL}>{this.props.userNickname}</a></strong>.</p>
+          <p>If you wish to log out, click the button below.</p>
+          <p className="text-center"><a className="btn btn-primary" href="/logout">Logout</a></p>
         </DefaultLayout>
       );
     }
