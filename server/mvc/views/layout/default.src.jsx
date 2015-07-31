46f21031d032f125d2d1ee5156bb309bfec77dae
kafoso <soefritz@gmail.com>
2015-07-31 18:32:17 +0200

    Improved some React logic for rendering meta tags and link tags

--- server/mvc/views/layout/default.src.jsx
+++ server/mvc/views/layout/default.src.jsx
@@ -2,11 +2,39 @@
 
 var React = require('react');
 
+var MetaDescription = React.createClass({
+  render: function(){
+    return(
+      this.props.description
+      ? <meta name="description" content={this.props.description}/>
+      : false
+    )
+  }
+});
+
+var MetaTag = React.createClass({
+  render: function(){
+    return (
+      <meta {...this.props.attributes} />
+    )
+  }
+});
+
+var LinkTag = React.createClass({
+  render: function(){
+    return (
+      <link type="text/css" rel="stylesheet" href={this.props.href}/>
+    )
+  }
+});
+
 var DefaultLayout = React.createClass({
   render: function() {
     var layout = typeof(this.props.layout) == "object" ? this.props.layout : {};
     var lang = layout.lang ? layout.lang : "en";
     var title = layout.title ? layout.title : "Starbow";
+    var metaOther = (layout.meta instanceof Array) ? layout.meta : [];
+    var css = (layout.css instanceof Array) ? layout.css : [];
     return (
       <html lang={lang}>
         <head>
@@ -14,32 +42,16 @@ var DefaultLayout = React.createClass({
           <title>{title}</title>
           <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
           <meta name="viewport" content="width=device-width,initial-scale=1"/>
-          {
-            layout.meta
-            ? layout.meta.map(function(attributes, index) {
-                return (
-                  <meta {...attributes} />
-                )
-              })
-            : null
-          }
-          {
-            layout.description
-            ? <meta name="description" content={layout.description}/>
-            : null
-          }
+          <MetaDescription description={layout.description}/>
+          {metaOther.map(function(attributes, index){
+            return <MetaTag attributes={attributes}/>
+          })}
           <link type="text/css" rel="stylesheet" href="/assets/cdn/bootstrap/3.3.1/css/bootstrap.min.css"/>
           <link type="text/css" rel="stylesheet" href="/assets/framework/bootstrap/themes/starbow.css"/>
           <link type="text/css" rel="stylesheet" href="/assets/layout/default.css"/>
-          {
-            layout.css
-            ? layout.css.map(function(href, index) {
-                return (
-                  <link type="text/css" rel="stylesheet" href={href}/>
-                )
-              })
-            : null
-          }
+          {css.map(function(href, index){
+            return <LinkTag href={href}/>
+          })}
         </head>
         <body>
           <page>
